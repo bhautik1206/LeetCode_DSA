5b08811564cee78f93c48f31b7c5488e5f57bcd1
Bhautik <bhautikkapadiya06@gmail.com>
2024-05-30 15:45:33 +0530

    All array question branch

---
+++ Array/ScoreOfAString3110.java
@@ -0,0 +1,12 @@
+// 3110. Score of a String
+
+class Solution {
+    public int scoreOfString(String s) {
+       int sum = 0;
+        int n = s.length();
+        for (int i = 0; i < n - 1; i++) {
+            sum += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return sum;
+    }
+}
